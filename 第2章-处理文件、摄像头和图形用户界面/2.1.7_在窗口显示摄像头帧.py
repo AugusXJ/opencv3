7464d2f17944993cf367ab6b11e53ab075aa6291
Augus <541468099@qq.com>
2018-06-12 20:07:28 +0800

    renew 2.1.7

--- 第2章-处理文件、摄像头和图形用户界面/2.1.7_在窗口显示摄像头帧.py
+++ 第2章-处理文件、摄像头和图形用户界面/2.1.7_在窗口显示摄像头帧.py
@@ -6,6 +6,8 @@
 import cv2
 
 clicked = False
+
+
 def onMouse(event, x, y, flags, param):
     """
     判断是否收到鼠标左键事件
@@ -20,9 +22,11 @@ def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONUP:                    # 如果获取鼠标左键事件
         clicked = True
 
+
+
 cameraCapture = cv2.VideoCapture(0)                     # 视频获取类
 cv2.namedWindow('MyWindows')
-cv2.setMouseCallback('MyWindow', onMouse)               # 获取键盘输入
+cv2.setMouseCallback('MyWindows', onMouse)               # 获取键盘输入
 print("showing camera feed. Click window or press any key to stop")
 success, frame = cameraCapture.read()
 while success and cv2.waitKey(1) == -1 and not clicked:
